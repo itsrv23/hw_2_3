cc62688b7719c5a596453a85f8c14a847433cdd9
itsrv23 <nifirus@gmail.com>
2022-05-03 12:29:12 +0300

    hw_2_3 Refactor

---
+++ src/main/java/ru/itsrv23/hwcalculator/servises/CalculatorService.java
@@ -0,0 +1,11 @@
+package ru.itsrv23.hwcalculator.servises;
+
+public interface CalculatorService {
+    Integer sum(Integer num1, Integer num2);
+
+    Integer subtract(Integer num1, Integer num2);
+
+    Integer multiply(Integer num1, Integer num2);
+
+    Integer divide(Integer num1, Integer num2);
+}
